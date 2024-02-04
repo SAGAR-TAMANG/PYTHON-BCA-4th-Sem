2fa8d2246f6e41353b9468c5d573713591d11733
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-02-04 14:46:56 +0530

    final commit with added license

---
+++ Jan 29th of Jan, 2024/BCA1.py
@@ -0,0 +1,7 @@
+fruits=["apple", "banana","mango"]
+for abc in fruits:
+    print(abc)
+
+for i in range(0, 21, 2):
+    print(i)
+
---
+++ Jan 29th of Jan, 2024/BCA2.py
@@ -0,0 +1,4 @@
+user = int(input("Give a number:"))
+
+for i in range(1, 11):
+    print(user, "*", i, " = ", user * i)
